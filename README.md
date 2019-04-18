cc2f425ba5cb5b8516d78098a06df39a3b84c2c2
Spencer McCoubrey <smccoubrey@verto.ca>
2019-04-17 22:41:44 -0400

    improved threading, added ram monitoring

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Simple MacOSX Process Moniter
 
-This is a simple python script that monitors cpu usage, and displays desktop notifications of possible errant processes, allowing the user to kill or ignore processes from the prompt
+This is a simple python script that monitors CPU and memory usage, and displays desktop notifications of possible errant processes, allowing the user to kill or ignore processes from the prompt
 
-## Setup 
+## Setup
 
-- script requires python 3.4.x (psutil support)
+- script requires python 3.4+ (psutil support)
 - create a python virtual environment for script (or just use base python if you prefer)
   - `pip install virtualenv && virtualenv <target> && source <target>/bin/activate`
 - `pip install -r requirements.txt` to install all requirements for script
 - currently using [terminal-notifier](https://github.com/julienXX/terminal-notifier) v1.8.0 for MacOSX desktop notifications
   - note version 1.8.0 is necessary to support actions and timeout
-  - future versions will hopefully use a python library for desktop notifications (or at least a more supportive notifier)
+  - future versions will hopefully use a python library for desktop notifications (or at least a more supported notifier)
   
 ## Usage
 
-`sudo python process_listener.py <desired system cpu usage: limit >= 0> <desired single process cpu usage: limit >= 0>`
+`sudo python process_listener.py <desired cpu usage: limit >= 0> <desired memory usage: limit >= 0>`
   
--- process_listener.py
+++ process_listener.py
@@ -3,73 +3,118 @@ import subprocess
 import ast
 import os
 import sys
-import threading
-
-PIDS = set()
+from threading import Thread, Lock
 
 
-class NotifyThread(threading.Thread):
-    def __init__(self, process, title, subtitle, message):
-        super(NotifyThread, self).__init__()
-        self.process = process
-        self.title = title
-        self.subtitle = subtitle
-        self.message = message
-        PIDS.add(self.process.info['pid'])
+class NotifyThreadHandler:
+    def __init__(self):
+        self.pids = set()
+        self.mutex = Lock()
 
-    def run(self):
-        # generate the required flags
-        m = '-message {!r}'.format(self.message)
-        t = '-title {!r}'.format(self.title)
-        st = '-subtitle {!r}'.format(self.subtitle)
-        a = '-actions {!r}'.format('Kill')
-        c = '-closeLabel {!r}'.format('Ignore')
-        ext = '-timeout 10 -json'
-        # generate notification and return the response object
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        raw_output = subprocess.check_output(
-            '{}/terminal-notifier {}'.format(dir_path, ' '.join([m, t, st, a, c, ext])), shell=True)
-        response_dict = ast.literal_eval(raw_output.decode('utf-8'))
-        # process response and kill if necessary
-        if response_dict.get('activationValue', '') == 'Kill':
-            try:
-                self.process.terminate()
-                PIDS.remove(self.process.info['pid'])
-            except psutil.NoSuchProcess as err:
-                print('Error:', err.msg, '--- while trying to terminate')
+    def generate_thread(self, process, title, subtitle, message):
+        pid = process.info['pid']
+        # don't run if process is being handled by another thread
+        if pid in self.pids:
+            return
+
+        # lock and add pid to blacklist
+        self.mutex.acquire()
+        self.pids.add(pid)
+        self.mutex.release()
+
+        # create notifier
+        thread = NotifyThreadHandler.NotifyThread(process, title, subtitle, message, self.remove_pid, (pid,))
+        thread.start()
+
+    def remove_pid(self, pid):
+        # acquire lock and remove pid from blacklist
+        self.mutex.acquire()
+        self.pids.remove(pid)
+        self.mutex.release()
+
+    class NotifyThread(Thread):
+        def __init__(self, process, title, subtitle, message, callback, callback_args=None):
+            super(NotifyThreadHandler.NotifyThread, self).__init__()
+            self.process = process
+            self.title = title
+            self.subtitle = subtitle
+            self.message = message
+            self.callback = callback
+            self.callback_args = callback_args
+
+        def run(self):
+            # generate the required flags
+            m = '-message {!r}'.format(self.message)
+            t = '-title {!r}'.format(self.title)
+            st = '-subtitle {!r}'.format(self.subtitle)
+            a = '-actions {!r}'.format('Kill')
+            c = '-closeLabel {!r}'.format('Ignore')
+            ext = '-timeout 10 -json'
+
+            # generate notification and return the response object
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            raw_output = subprocess.check_output(
+                '{}/terminal-notifier {}'.format(dir_path, ' '.join([m, t, st, a, c, ext])), shell=True)
+            response_dict = ast.literal_eval(raw_output.decode('utf-8'))
+
+            # process response and kill if necessary
+            if response_dict.get('activationValue', '') == 'Kill':
+                try:
+                    self.process.terminate()
+                except psutil.NoSuchProcess as err:
+                    print('Error:', err.msg, '--- while trying to terminate')
+            # callback function
+            self.callback(*self.callback_args)
 
 
 # runner logic
 if __name__ == '__main__':
-    usage = 'Usage: sudo python process_listener.py <desired system cpu usage: limit >= 0> ' \
-            '<desired single process cpu usage: limit >= 0>'
+    usage = 'Usage: sudo python process_listener.py <desired cpu usage: limit >= 0> ' \
+            '<desired memory usage: limit >= 0>'
     # checking args
     if len(sys.argv) < 3:
         print(usage)
         exit(1)
     try:
-        system_usage_limit, single_usage_limit = int(sys.argv[1]), int(sys.argv[2])
+        cpu_usage_limit, mem_usage_limit = int(sys.argv[1]), int(sys.argv[2])
     except ValueError as e:
         print(usage)
         exit(1)
-    if system_usage_limit < 0 or single_usage_limit < 0:
+    if cpu_usage_limit < 0 or mem_usage_limit < 0:
         print(usage)
         exit(1)
 
-    # currently only checking cpu usage
     cpu_count = psutil.cpu_count(True)
-    print('success: started listening to cpu usage...')
+    notifier_handler = NotifyThreadHandler()
+    print('success: started listening to memory and CPU usage...')
     while True:
         # get current usage
         cpu_usage = sum(psutil.cpu_percent(interval=1, percpu=True))
-        ram_usage = psutil.virtual_memory().percent
+        mem_usage = psutil.virtual_memory().percent
 
         # if cpu usage is more than desired percent of system
-        if cpu_usage >= (cpu_count * system_usage_limit):
+        if cpu_usage >= (cpu_count * cpu_usage_limit):
             # check all currently running processes
             for proc in psutil.process_iter(attrs=['pid', 'name', 'cpu_percent']):
-                # if large process (defined by user), notify user, kill if responded
-                if proc.info['pid'] not in PIDS and proc.info['cpu_percent'] >= single_usage_limit:
-                    notify = NotifyThread(proc, 'Process Listener', '',
-                                          proc.info['name'] + ' at CPU USAGE: ' + str(proc.info['cpu_percent']) + '%')
-                    notify.start()
+                cpu_percent = proc.info['cpu_percent']
+                if cpu_percent is None:
+                    continue
+
+                # notify user of large process, kill if responded
+                if cpu_percent >= 40:
+                    notifier_handler.generate_thread(proc, 'Process Listener', proc.info['name'],
+                                                     f'CPU usage at: {str(cpu_percent)}%')
+
+        # if memory usage is more than desired percent of system
+        elif mem_usage >= mem_usage_limit:
+            print(f'{mem_usage} >= {mem_usage_limit}')
+            # check all currently running processes
+            for proc in psutil.process_iter(attrs=['pid', 'name']):
+                mem_percent = proc.memory_percent()
+                if mem_percent is None:
+                    continue
+
+                # notify user of large process, kill if responded
+                if mem_percent >= 20:
+                    notifier_handler.generate_thread(proc, 'Process Listener', proc.info['name'],
+                                                     f'RAM usage at: {str(mem_percent)}%')
